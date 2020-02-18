6fd35b98b5ab0729244b2e16817e1fe01aca7202
nirpale <ninabrunou@gmail.com>
2020-02-18 11:00:43 +0200

    changed readme and added comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
-import Display from "./Display";
+import Display from "./Display"; //import for the component with the actual code in it
+
 
 class App extends Component {
+
+    //the app.js renders and returns the display component and its code
+
   render() {
     return (
         <div className="App">
--- src/Display.js
+++ src/Display.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import data from "./restaurants.json";
+import data from "./restaurants.json"; //importing the local .json file
 import Card from '@material-ui/core/Card';
 import {Button, CardContent, CardHeader, CardMedia, Grid, Typography} from "@material-ui/core";
 
@@ -7,14 +7,17 @@ class Display extends Component {
 
     constructor(props) {
         super(props)
-        this.sortAtoZ = this.sortAtoZ.bind(this)
+        this.sortAtoZ = this.sortAtoZ.bind(this) //binds the function so that the 'this' value is passed to the function once it's called
         this.sortZtoA = this.sortZtoA.bind(this)
         this.state = {
-            restList : []
+            restList : [] //initializing empty array as the state
         }
     }
 
     sortAtoZ(event){
+        //sorting function that's run once the button is pressed. Sorts the array aplhabetically according to name value
+        //and sets the sorted array to the state
+
         const {restList} = this.state
         let newRestList = restList
         restList.sort((a,b) => (a.name > b.name) ? 1 : -1)
@@ -24,6 +27,8 @@ class Display extends Component {
     }
 
     sortZtoA(event){
+        //function that reverses the array order based on name once button is pressed and returns it as a state
+
         const {restList} = this.state
         let newRestList = restList
         restList.reverse()
@@ -33,7 +38,7 @@ class Display extends Component {
     }
 
         componentDidMount() {
-        const restList = data
+        const restList = data //the empty array which gets the data from the imported .json file
         this.setState({
             restList:  restList.restaurants
         })
@@ -48,7 +53,8 @@ class Display extends Component {
                 <Button variant="outlined" color="primary" size="large" style={{padding: 10, marginLeft: '38%', marginRight: 5, marginBottom: 50, marginTop: 20}} onClick={this.sortAtoZ}>Sort ascending</Button>
                 <Button variant="outlined" color="secondary" size="large" style={{padding: 10, marginLeft: 5,marginBottom: 50, marginTop: 20}} onClick={this.sortZtoA}>Sort descending</Button>
             <Grid container spacing = {30} flewGrow={3} justifyContent = 'row' >
-                {restList.map((restaurant, i) => {
+                {restList.map((restaurant, i) => { //function which goes through the .json file, defines each object with unique key
+                                                    // and shows information defined below
                         return <Grid key={i} >
                             <Card style={{width: 400, height: 360, marginLeft: 50, marginRight: 50, marginBottom: 50, marginTop: 10}} variant="outlined">
                             <CardHeader title={restaurant.name}
@@ -69,4 +75,4 @@ class Display extends Component {
     }
 }
 
-export default Display;
+export default Display; //exports the component and its functions for app.js to use
